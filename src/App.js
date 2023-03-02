cf11dc877a3e85552d43d73d6296652544988145
Mohammed Abubakar <mohammedfahim363@yahoo.com>
2023-03-01 21:56:18 -0800

    lab3

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
 import React from 'react';
 import Main from './Main';
+import Header from './Header';
+import Footer from './Footer';
 import data from './data.json';
+import Modal from 'react-bootstrap/Modal';
 import './App.css';
 
 
 class App extends React.Component {
-  // inside render is the html jtsx
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+      title: '',
+      url: '',
+      description: '',
+    }
+  }
+
+  handleCloseModal = () => {
+    this.setState({
+      showModal: false
+    });
+  }
+
+  handleOpenModal = (title, imageUrl, description) => {
+    this.setState({
+      showModal: true,
+      title: title,
+      url: imageUrl,
+      description: description,
+    });
+  }
+
   render() {
-  return (
-    // wrap for no sibling elements
-  <>
-  <header><h1>Horned Beasts of Wrestling</h1></header>
-<Main data={data}/>
-
-
-  </>)
- }
+    return (
+      // wrap for no sibling elements
+      <>
+        <Header/>
+        <Main data={data} handleOpenModal={this.handleOpenModal} />
+        <Footer/>
+        <Modal
+        show={this.state.showModal}
+        onHide={this.handleCloseModal}
+        >
+        <Modal.Header closeButton>
+          <Modal.Title>{this.state.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body><img src={this.state.url} alt={this.state.title}/></Modal.Body>
+        <Modal.Footer>{this.state.description}</Modal.Footer>
+        </Modal>
+      </>
+      );
+  }
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+class Header extends React.Component{
+  render(){
+    return ( <header><h1>Horned Beasts</h1></header>)
+  }
+}
+
+export default Header;
